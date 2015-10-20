c5702f27a03262092448e9fc89a2db3c5f40481b
陈宁可 <972478710@qq.com>
2015-10-20 21:03:09 +0800

    20151020

--- exercise.cpp
+++ exercise.cpp
@@ -2,6 +2,10 @@
 #include<cctype>
 #include<string>
 #include<bitset>
+#include<vector>
+#include <stdarg.h>
+#include<cmath>
+using std::vector;
 using namespace std;
 int get_value()
 {
@@ -62,7 +66,7 @@ cout<<int_quiz1<<endl;
 bitset_quiz1.reset(27);
 cout<<bitset_quiz1<<endl;
 int_quiz1&=~(1UL<<27);
-bool status,status1;
+bool status;
 status=bitset_quiz1[27];
 cout<<status<<"  "<<endl;
 return 0;
@@ -80,8 +84,110 @@ string p1=s+(s[s.size ()-1]=='s'?"":"s");
 cout<<p1<<endl;
 return 0;
 }
-int main()
+int p151()//new and delete for space
 {int i(1024);
 int *pi=new int (1024);
 string s(10,'9');
-string *ps=new string (10,'9');}
+string *ps=new string (10,'9');
+cout<<*pi<<"  "<<pi<<"  "<<*ps<<"   "<<ps<<endl;
+delete pi;
+delete ps;
+cout<<pi<<"  "<<ps<<endl;
+return 0;}
+int p176_6_7()
+{char ch;
+int aCnt=0,eCnt=0,iCnt=0,oCnt=0,uCnt=0;
+while(cin>>ch){cout<<ch;
+switch(ch){
+case 'a':
+case 'A':
+	++aCnt;
+	break;
+case 'e':
+case 'E':
+	++eCnt;
+	break;
+case 'i':
+case 'I':
+	++iCnt;
+	break;
+case 'o':
+case 'O':
+	++oCnt;
+	break;
+}cout<<"aCnt="<<aCnt<<"eCnt="<<eCnt<<"iCnt="<<iCnt<<"oCnt="<<oCnt<<"uCnt="<<uCnt<<endl;
+}
+return 0;}
+int daixiugai()
+{
+	string word1,word2;
+vector<string>vec1;
+vector<string>vec2;
+cout<<"输入第一个向量值";
+while(cin>>word1){
+	cout<<word1;
+	vec1.push_back(word1);break;}
+cout<<"输入第二个向量值";
+while(cin>>word1){
+	vec2.push_back(word2);break;}
+int sz=vec1.size()<=vec2.size ()?vec1.size():vec2.size ();
+for(int i=0;i!=sz;i++)
+	if(vec1==vec2)cout<<"shiqinazhui";
+	else{
+		cout<<"短的不是长的前缀！";break;}
+	return 0;
+}
+int P182_6_9()
+{string rsp;
+do
+{cout<<"Please enter two values:";
+int val1,val2;
+cin>>val1>>val2;
+cout<<"Sum of"<<val1<<"and"<<val2<<"="<<val1+val2<<"\n\n"<<"more?[yes][no]";
+cin>>rsp;}
+while(!rsp.empty()&&rsp[0]!='n');return 0;}
+int p183_6_8()
+{
+	string val1,val2;do{
+cout<<"shurudi1gezhi.";
+cin>>val1;
+cout<<"shurudi2gezhi.";
+cin>>val2;
+cout<<val1<<"  "<<val2<<endl;
+const char *p1=&val1[0];
+const char *p2=&val2[0];
+
+	int i=strcmp(p1,p2);
+	cout<<(i<0?p1:p2)<<"更小";
+}while(val1[0]);
+return 0;
+}
+int gcd(int v1,int v2)
+{int tempor;
+{while (v2)
+ tempor=v2;
+v2=v1%v2;
+v1=tempor;}
+return v1;
+}
+int	Get_MI(int a,int b)
+{
+	int exp=1;
+	for(int i=0;i<b;i++)
+exp*=a;
+return exp;}
+double abs1(double a)
+{return(a>=0?a:-a);}
+int exchang(int &a,int &b)//引用实现交换
+{int temp;
+temp=b;
+b=a;
+a=temp;
+return a,b;}
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	exchang(a,b);
+cout<<a<<b;
+return 0;}
