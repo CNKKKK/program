622b26e1eeb28d5949856aeb14eab53b821185c8
陈宁可 <972478710@qq.com>
2017-01-14 22:52:00 +0800

    2017.01.14

--- exercise.cpp
+++ exercise.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cctype>
 #include<string>
+#include<cstring>
 #include<bitset>
 #include<vector>
 #include<list>
@@ -381,4 +382,43 @@ int exercise3_2()
 
 	cout << endl;
 	return (0);
+}
+int arrayone(){
+	int yams[3];
+	yams[0] = 7;
+	yams[1] = 8;
+	yams[2] = 6;
+	
+	int yamcosts[3] = { 20, 30, 5 };
+
+	cout << "Total yams =";
+	cout << yams[0] + yams[1] + yams[2] << endl;
+	cout << "The package with" << yams[1] << " yams counts";
+	cout << yamcosts[1] << " cents peryam.\n";
+	int total = yams[0] * yamcosts[0] + yams[1] * yamcosts[1];
+	total = total + yams[2] * yamcosts[2];
+	cout << "The total yam expence is  " << total << " cents\n";
+	cout << "\nSize of yams array =" << sizeof yams;
+	cout << "bytes.\n";
+	cout << "Size of one element =" << sizeof yamcosts[0];
+	cout << "bytes.\n";
+	return 0;
+}
+int main()
+{
+	const int Size = 15;
+	char name1[Size];
+	char name2[Size] = "C++owboy";
+
+	cout << "Howdy!I'm" << name2;
+	cout << "! What 's your name?\n";
+	cin >> name1;
+	cout << "Well, " << name1 << ", your name has ";
+	cout << strlen(name1) << " letters and is stored\n";
+	cout << "in an array of " << sizeof(name1) << " bytes.\n";
+	cout << "Your initial is " << name1[0] << ".\n";
+	name2[3] = '\0';
+	cout << "Here are the first 3 characters of my name: ";
+	cout << name2 << endl;
+	return 0;
 }
