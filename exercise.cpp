0d0fc9d3cdcc5ce3605f04175ae9826c94db44f3
陈宁可 <972478710@qq.com>
2017-01-16 22:24:21 +0800

    2017.01.16

--- exercise.cpp
+++ exercise.cpp
@@ -435,7 +435,7 @@ int instr_1(){
 	cout << " for you, " << name << " .\n";
 	return 0;
 }
-int main(){
+int instr_2(){
 	const int ArSize = 20;
 	char name[ArSize];
 	char dessert[ArSize];
@@ -446,5 +446,58 @@ int main(){
 	cin.getline(dessert, ArSize);
 	cout << "I have some delicious " << dessert;
 	cout << " for you, " << name << " .\n";
+	return 0;
+}
+int numberins(){
+	cout << "What year was your house built.\n";
+	int year;
+	cin >> year;
+	cout << "What is its street address?\n";
+	char address[80];
+	cin.getline(address, 80);
+	cout << "Year built: " << year << endl;
+	cout << "Adress: " << address << endl;
+	cout << "Done!\n";
+	return 0;
+
+}
+int instring(){
+
+	char charr1[20];
+	char charr2[20] = "jaguar"; 
+	string str1;
+	string str2 = "panther";
+
+	cout << "Enter a kind of feline: ";
+	cin >> charr1;
+	cout << "Enter another kind of feline: ";
+	cin >> str1;
+	cout << "Here are some felines: \n";
+	cout << charr1 << " " << charr2 << " " << str1 << " " << str2 << endl;
+	cout << "The third letter in " << charr2 << " is " << charr2[2] << endl;
+	cout << "The third letter in " << str2 << " is " << str2[2] << endl;
+
+	return 0;
+}
+int main(){
+	string s1 = "penguin";
+	string s2, s3;
+
+	cout << "You can assign one string object to another:s2=s1\n ";
+	s2 = s1;
+	cout << "s1:" << s1 << ", s2:" << s2 << endl;
+	cout << "You can assign a C-style string to a string object.\n";
+	cout << "s2=\"buzzard\"\n";
+	s2 = "buzzard";
+	cout << "s2;" << s2 << endl;
+	cout << "You can concatenate strings:s3= s2+s1\n";
+	cout << "s3:" << s3 << endl;
+	s1 += s2;
+	cout << "You can append atrings.\n";
+	s2 += "for a day";
+	cout << "s1+=s2 yields s1 =" << s1 << endl;
+	s2 += "for a day";
+	cout << "s2+= \"for a day\"yields s2 =" << s2 << endl;
+
 	return 0;
 }
